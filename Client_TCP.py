f4b439dfafe0b6c0bb262eb7ac34b8c9389f7c47
AndreBa07 <andre.bagnis7@gmail.com>
2025-11-13 10:05:18 +0100

    Implement keyboard controls and command execution
    Added keyboard control functionality and command reading from database.

--- Client_TCP.py
+++ Client_TCP.py
@@ -1,4 +1,7 @@
 import socket
+from pynput import keyboard
+import sqlite3 as sql
+import time
 
 SERVER_ADDRESS = ("192.168.1.117", 5000) # Indirizzo IP del server al quale mando un messaggio e sua porta
 
@@ -6,10 +9,52 @@ SERVER_ADDRESS = ("192.168.1.117", 5000) # Indirizzo IP del server al quale mand
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # STREAM = TCP
 s.connect(SERVER_ADDRESS)
 
-n = 5
-for _ in range(n):
-    istruzione = input("Inserisci una direzione in cui muovere il robot (left, right, forward, backward): ")
-    s.send(istruzione.encode())
-    """tempo = input("Inserisci per quanto tempo eseguire l'istruzione: ")
-    s.send(tempo.encode())"""
-s.close()
+def leggi_comandi(nome, carattere):
+    con = sql.connect(nome)
+    cur = con.cursor()
+    sequenza_file = cur.execute(f"SELECT Sequenza FROM movimenti WHERE Tasto = ?", (carattere, )) # WHERE val = '{x}'
+    sequenza = sequenza_file.fetchall()
+    singole_istruzioni = sequenza[0][0].split(",")
+
+    for i in range(0, len(singole_istruzioni), 2):
+        istruzione = "DB" + singole_istruzioni[i]
+        tempo = float(singole_istruzioni[i + 1])
+        print(f"{istruzione}")
+        s.send(istruzione.encode())
+        time.sleep(tempo)
+        s.send("stop".encode())
+
+    con.close()
+
+def on_press(key):
+    try:
+        if key.char == "w":
+            s.send("w".encode())
+        elif key.char == "a":
+            s.send("a".encode())
+        elif key.char == "s":
+            s.send("s".encode())
+        elif key.char == "d":
+            s.send("d".encode())
+        else:
+            leggi_comandi("movimenti_Larry.db", key.char)
+
+    except AttributeError:
+        if key == keyboard.Key.up:
+            s.send("w".encode())
+        elif key == keyboard.Key.left:
+            s.send("a".encode())
+        elif key == keyboard.Key.down:
+            s.send("s".encode())
+        elif key == keyboard.Key.right:
+            s.send("d".encode())
+        elif key == keyboard.Key.esc:
+            s.send("finish".encode())
+            s.close()
+            return False
+
+def on_release(key):
+    s.send("stop".encode())
+
+with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
+    listener.join()
